f771594bbc2963ad6626a2e5f0b5eb82fc1a87e7
Adi Magori <adimagori@gmail.com>
2021-01-13 22:20:13 +0200

    Adi M. card details

--- src/cmps/CardDescription.jsx
+++ src/cmps/CardDescription.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
+// import Icon from '@material-ui/core/Icon';
 
 export function CardDescription() {
     return (
         <div className="">
             <div className="flex">
-                <icon>=</icon>
+                {/* <Icon>Subject</Icon> */}
+
                 <h3>Description</h3>
             </div>
             <textarea placeholder='Add a more detailed description...' rows="4" cols="50"></textarea>
             <div className="flex">
                 <button>Save</button>
-                <icon>=</icon>
-
             </div>
         </div>
     )
--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 export class Header extends Component {
   render() {
     return (
       <div className="header-wrapper">
-        <FontAwesomeIcon icon={['fas','home']} />
         <Link className="header-btn">Boards</Link>
-        <input className="header-input" type="text"/>
+        <input className="header-input" type="text" />
         <Link className="logo">Cardly</Link>
         <Link>Username</Link>
       </div>
