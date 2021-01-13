4bff3fc21043d7847734af529557096b570ee720
mmts12 <56490587+mmts12@users.noreply.github.com>
2021-01-13 21:11:54 +0200

    add stor to Board

--- src/store/reducers/boardReducer.js
+++ src/store/reducers/boardReducer.js
@@ -1,11 +1,14 @@
 const initialState = {
-    boards: []
+    boards: [],
+    selectedBoard: {}
 }
 
 export function boardReducer(state = initialState, action) {
     switch (action.type) {
         case 'SET_BOARDS':
             return { ...state, boards: action.boards }
+        case 'SET_BOARD':
+            return { ...state, selectedBoard: action.board }
 
         default:
             return state
