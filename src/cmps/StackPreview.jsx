d9c27737e9b5c1c80f503c90a7c07eaea1868d5e
mmts12 <56490587+mmts12@users.noreply.github.com>
2021-01-15 12:15:48 +0200

    try to delete stack

--- src/cmps/StackPreview.jsx
+++ src/cmps/StackPreview.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import { CardList } from './CardList';
 import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { connect } from 'react-redux';
+import { removeStack } from '../store/actions/stackActions.js';
+import { setSelectedBoard } from '../store/actions/boardActions';
 
-export class StackPreview extends Component {
+export class _StackPreview extends Component {
+  onRemoveStack = () => {
+    const { stack, selectedBoard, removeStack } = this.props;
+    removeStack(stack.id, selectedBoard._id, selectedBoard);
+  };
+
+  // componentDidMount() {
+  //   this.loadBoard();
+  // }
+
+  // loadBoard = () => {
+  //   const { selectedBoard } = this.props;
+  //   this.props.setSelectedBoard(selectedBoard);
+  // };
+
   render() {
     const { stack } = this.props;
     return (
@@ -10,9 +28,29 @@ export class StackPreview extends Component {
         <h3 className="stack-title">
           {stack.title}
           <EditIcon className="stack-preview-edit-icon"></EditIcon>
+          <div onClick={this.onRemoveStack}>
+            <DeleteIcon className="stack-preview-delete-icon"></DeleteIcon>
+          </div>
         </h3>
         <CardList cards={stack.cards} />
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    selectedBoard: state.boardModule.selectedBoard,
+    boards: state.boardModule.boards,
+  };
+};
+
+const mapDispatchToProps = {
+  removeStack,
+  setSelectedBoard,
+};
+
+export const StackPreview = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(_StackPreview);
--- src/store/actions/stackActions.js
+++ src/store/actions/stackActions.js
@@ -12,3 +12,21 @@ export function addStack(stack, boardId) {
             })
     }
 }
+export function removeStack(stackId, boardId, selectedBoard) {
+    return (dispatch) => {
+        return boardService.removeStack(stackId, boardId, selectedBoard)
+            .then((board) => {
+
+                const action = {
+                    type: 'REMOVE_BOARD',
+                    stackId,
+                }
+                dispatch(action)
+                // const action1 = {
+                //     type: 'UPDATE_BOARD',
+                //     board,
+                // }
+                // dispatch(action1)
+            })
+    }
+}
